6d49e5542d014b3d01faadc3023527d67c5f61a8
Theodor <tp@atelierstromberg.de>
2024-05-23 15:05:37 +0200

    Added method FindUserByEmail in class UserDao

--- src/main/java/org/hbrs/se2/project/hellocar/dao/UserDAO.java
+++ src/main/java/org/hbrs/se2/project/hellocar/dao/UserDAO.java
@@ -5,6 +5,7 @@ import org.hbrs.se2.project.hellocar.dtos.UserDTO;
 import org.hbrs.se2.project.hellocar.dtos.impl.UserDTOImpl;
 import org.hbrs.se2.project.hellocar.services.db.JDBCConnection;
 import org.hbrs.se2.project.hellocar.services.db.exceptions.DatabaseLayerException;
+import org.hbrs.se2.project.hellocar.util.AccountType;
 import org.hbrs.se2.project.hellocar.util.Globals;
 
 import java.sql.ResultSet;
@@ -82,8 +83,69 @@ public class UserDAO {
         }
     }
 
-    public UserDTO FindUserByEmail(String email) { //throws DatabaseLayerException
+    public UserDTO FindUserByEmail(String email) throws DatabaseLayerException {
         UserDTO user = new UserDTOImpl();
-        return user;
+        // Set ResultSet to null;
+        ResultSet set = null;
+
+        // Set try-clause
+        try {
+            Statement statement = null;
+            try {
+                statement = JDBCConnection.getInstance().getStatement();
+            } catch (DatabaseLayerException e) {
+                e.printStackTrace();
+            }
+
+            set = statement.executeQuery(
+                    "SELECT * "
+                            + "FROM collabhbrs.users "
+                            + "WHERE collabhbrs.users.email = \'" + email + "\'");
+
+            // JDBCConnection.getInstance().closeConnection();
+
+        } catch (SQLException ex) {
+            DatabaseLayerException e = new DatabaseLayerException("Fehler im SQL-Befehl!");
+            e.setReason(Globals.Errors.SQLERROR);
+            throw e;
+        }
+        catch (NullPointerException ex) {
+            DatabaseLayerException e = new DatabaseLayerException("Fehler bei Datenbankverbindung!");
+            e.setReason(Globals.Errors.DATABASE);
+            throw e;
+        }
+
+        try {
+            if (set.next()) {
+                // Durchführung des Object-Relational-Mapping (ORM)
+                user.setId( set.getInt("id"));
+                user.setUserId(set.getString("userid"));
+                user.setEmail( set.getString("email") );
+                user.setPassword(set.getString("password"));
+                user.setAccountType(AccountType.valueOf(set.getString("accounttype")));
+
+                // Beziehe die Rollen eines Users:
+                RolleDAO rolleDAO = new RolleDAO();
+                List<RolleDTO> rollen = rolleDAO.getRolesOfUser(user);
+
+                // Einsetzen der Rollen in ein User-Object
+                user.setRoles(rollen);
+
+                return user;
+
+            } else {
+                // Error Handling
+                DatabaseLayerException e = new DatabaseLayerException("No User Could be found");
+                e.setReason(Globals.Errors.NOUSERFOUND);
+                throw e;
+            }
+        } catch (SQLException ex) {
+            DatabaseLayerException e = new DatabaseLayerException("Probleme mit der Datenbank");
+            e.setReason(Globals.Errors.DATABASE);
+            throw e;
+
+        } finally {
+            JDBCConnection.getInstance().closeConnection();
+        }
     }
 }
