dae9eea21ce704a73790ce7fddc1925f5a632ff5
Jan <jan.alexander.kaminski@gmx.de>
2024-05-28 13:23:14 +0200

    Teststruktur erstellt

---
+++ src/test/java/FreshConnect/Test/RegistrationTest.java
@@ -0,0 +1,16 @@
+package FreshConnect.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+public class RegistrationTest {
+
+}
